4ad541bb0f21d5518f7eb6996a5012c794779915
Akmal Ismoilov <master80098@gmail.com>
2022-07-27 17:25:33 +0500

    Task completed!

--- src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
+++ src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task5;
 
 public class ThreeStuckVars {
     public static void main(String[] args) {
+        int first = 1, seconds = 10, third = 100;
+        System.out.println(String.valueOf(first) + seconds + third);
     }
 }
